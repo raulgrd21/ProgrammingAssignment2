707445b513f8d3277e88a76f7eee3f81dce76b66
raulgrd21 <raulgrd21@gmail.com>
2017-07-16 22:53:09 -0500

    Cahcematrix Comments
    The list created by makeCacheMatrix allows to acces the entire function's envirornment. This is key to being able to retreive the inverse.
    Also by setting names to the list, we can acces the functions by name. This is very usefull in cacheSolve when it is necessary to set a new inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function caches the inverse of a given matrix to save costly computations
+## It works by creating a an object that allows access to the complete environment of makeCacheMatrix
+## when the functions has stoped running, this enable to store the inverse of the matrix that cacheSolve will
+## will calculate only once, and retreive without having to recalculate it
 
 makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) inv <<- solve
+    getinv <- function() inv
+    list(set = set, get = get,
+    setinv = setinv,
+    getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## This functions checks if the inverse has been already calculated. If it has, it then retreives it, if it
+## hasn't it calculates it and caches it by calling its setter, which in turn stores the new value "inv" in the
+## makeCacheMatrix's envirornment allowing its further access without recalculation
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
 }
